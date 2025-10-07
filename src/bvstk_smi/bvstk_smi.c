c3a46d4b7288692e3ff21dfc18de2d100ec7dabd
ilya_t <ig.trefilov@mail.ru>
2025-10-07 10:18:15 +0300

    Add some xil_printf

--- src/bvstk_smi/bvstk_smi.c
+++ src/bvstk_smi/bvstk_smi.c
@@ -1,6 +1,6 @@
 #include "bvstk_smi.h"
 
-uint8_t phy_addr   = 0x01;
+//uint8_t phy_addr   = 0x01;
 uint32_t bram_master_data = 0;
 uint32_t bram_master_addr = 0;
 uint32_t bram_slave_data = 0;
@@ -28,10 +28,10 @@ void smi_task(void *pvParameters)
 
         while (1) {
                 for (uint8_t i = 0; i < 32; i++) {
-                        mdio_read(phy_addr, i);
+                        mdio_read(0x01, i);
                 }
                 vTaskDelay(pdMS_TO_TICKS(1000));
-                xil_printf("32 registers were read from PHY\n\r");
+                //xil_printf("32 registers were read from PHY\n\r");
         }
 }
 
@@ -71,7 +71,7 @@ uint16_t timeout_read()
 
 static void master_ISR(void *CallBackRef) {
     uint32_t csr = Xil_In32(MASTER_BASEADDR + CSR_m);
-    xil_printf("[IRQ master]: CSR = 0x%08x,\t", csr);
+    //xil_printf("[IRQ master]: CSR = 0x%08x,\t", csr);
 
     if ((csr & 0x28)) { // Если один из буферов заполнен, сбрасываем систему
         Xil_Out32(MASTER_BASEADDR + CSR_m, 0x01);
@@ -82,7 +82,7 @@ static void master_ISR(void *CallBackRef) {
         bram_master_data = Xil_In32(bram_master_addr);
         Xil_Out32(bram_master_addr + SLAVE_RD_OFFSET, bram_master_data); // Перекладываем эти данные в область slave read, чтобы host мог их прочитать
         Xil_Out32(MASTER_BASEADDR + IRQ_m, 1); // Сброс IRQ
-        xil_printf("| addr = 0x%08x | data = 0x%08x\n\r", bram_master_addr, bram_master_data);
+        //xil_printf("[IRQ master]: | addr = 0x%08x | data = 0x%08x\n\r", bram_master_addr, bram_master_data);
         return;
     }
     //xil_printf("\n\r");
@@ -94,12 +94,28 @@ static void slave_ISR(void *CallBackRef) {
     if ((csr & 0x01)) { // Если хост записал данные, обрабатываем их и если всё норм, передаем их на запись в ядро master
         Xil_Out32(MASTER_BASEADDR + CSR_m, 0x01);
         bram_slave_addr = Xil_In32(SLAVE_BASEADDR + MEM_ADDR_s);
+        uint32_t phy_reg_field = (bram_slave_addr - BRAM_BASEADDR - SLAVE_WR_OFFSET) / 4;
+        uint8_t reg_addr = phy_reg_field & 0x1F;
+        uint8_t phy_addr = phy_reg_field >> 5;
         bram_slave_data = Xil_In32(bram_slave_addr);
-        uint8_t reg_addr = bram_slave_addr & 0x1F;
+        uint16_t data = bram_slave_data & 0xFFFF;
+        xil_printf("[IRQ slave]: Host write data = 0x%04X reg_addr=%u phy_addr=%u \n\r", data, reg_addr, phy_addr);
         mdio_write(phy_addr, reg_addr, bram_slave_data & 0xFFFF);
-        //xil_printf("[IRQ slave]: | addr = 0x%08x | data = 0x%08x\n\r", bram_slave_addr, bram_slave_data);
+
     } else if (csr & 0x04) { // Если хост считал данные из BRAM, то просто оповещаем об этом проц
         slave2host_data = Xil_In32(SLAVE_BASEADDR + S2H);
+
+        uint16_t data      =  slave2host_data        & 0xFFFF;       // биты [15:0]
+        uint8_t  reg_addr  = (slave2host_data >> 16) & 0x1F;         // биты [20:16]
+        uint8_t  phy_addr  = (slave2host_data >> 21) & 0x1F;         // биты [25:21]
+        uint8_t  rw        = (slave2host_data >> 26) & 0x01;         // бит  [26]
+
+        xil_printf("[IRQ slave]: Host read data=0x%04X reg_addr=%u phy_addr=%u %s\r\n",
+                   data,
+                   reg_addr,
+                   phy_addr,
+                   rw ? "write" : "read");
+
         //xil_printf("slave2host_data = 0x%08x\n\r", slave2host_data);
     }
     //xil_printf("[IRQ slave]: | addr = 0x%08x | data = 0x%08x\n\r", bram_slave_addr, bram_slave_data);
